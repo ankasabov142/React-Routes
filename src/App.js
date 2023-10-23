cf9bb9565ddc0a1bcf78d7250a7e15b0fe4a58db
Angel Kasabov <ankasabov142@gmail.com>
2023-10-23 11:37:20 +0300

    Updated calculator module

--- src/App.js
+++ src/App.js
@@ -7,34 +7,36 @@ function App() {
         <h1>Calculator</h1>
     
         <table>
+          <tbody>
           <tr>
             <td>Res:</td>
             <td id="textResult">0</td>
-            <td><button id="buttonEqual" onclick="operators('=')">=</button></td>
+            <td><button id="buttonEqual" onClick="operators('=')">=</button></td>
           </tr>
           <tr>
-            <td><button onclick="numbers('1')" id="buttonOne">1</button></td>
-            <td><button onclick="numbers('2')" id="buttonTwo">2</button></td>
-            <td><button onclick="numbers('3')" id="buttonThree">3</button></td>
-            <td><button onclick="operators('/')" id="buttonDivision">/</button></td>
+            <td><button onClick='numbers("1")' id="buttonOne">1</button></td>
+            <td><button onClick="numbers('2')" id="buttonTwo">2</button></td>
+            <td><button onClick="numbers('3')" id="buttonThree">3</button></td>
+            <td><button onClick="operators('/')" id="buttonDivision">/</button></td>
           </tr>
           <tr>
-            <td><button onclick="numbers('4')" id="buttonFour">4</button></td>
-            <td><button onclick="numbers('5')" id="buttonFive">5</button></td>
-            <td><button onclick="numbers('6')" id="buttonSix">6</button></td>
-            <td><button onclick="operators('*')" id="buttonMultiplication">*</button></td>
+            <td><button onClick="numbers('4')" id="buttonFour">4</button></td>
+            <td><button onClick="numbers('5')" id="buttonFive">5</button></td>
+            <td><button onClick="numbers('6')" id="buttonSix">6</button></td>
+            <td><button onClick="operators('*')" id="buttonMultiplication">*</button></td>
           </tr>
           <tr>
-            <td><button onclick="numbers('7')" id="buttonSeven">7</button></td>
-            <td><button onclick="numbers('8')" id="buttonEight">8</button></td>
-            <td><button onclick="numbers('9')" id="buttonNine">9</button></td>
-            <td><button onclick="operators('+')" id="buttonAddition">+</button></td>
+            <td><button onClick="numbers('7')" id="buttonSeven">7</button></td>
+            <td><button onClick="numbers('8')" id="buttonEight">8</button></td>
+            <td><button onClick="numbers('9')" id="buttonNine">9</button></td>
+            <td><button onClick="operators('+')" id="buttonAddition">+</button></td>
           </tr>
           <tr>
-            <td><button onclick="operators('-')" id="buttonSubtracting">-</button></td>
-            <td><button onclick="numbers('0')" id="buttonZero">0</button></td>
-            <td><button onclick="numbers('.')" id="buttonDot">.</button></td>
+            <td><button onClick="operators('-')" id="buttonSubtracting">-</button></td>
+            <td><button onClick="numbers('0')" id="buttonZero">0</button></td>
+            <td><button onClick="numbers('.')" id="buttonDot">.</button></td>
           </tr>
+          </tbody>
         </table>
         <script defer src="Calculator.js"></script>
 </div>
