575297700a702db257f63af4437b785c85803af9
Angel Kasabov <ankasabov142@gmail.com>
2023-11-06 00:07:51 +0200

    Add api file

---
+++ src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://127.0.0.1:8000/api";
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const registerUser = async (username, email, password) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/register`, {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Implement other API functions for user info, car management, etc.
